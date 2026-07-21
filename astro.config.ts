afeb3f63a3c6d008b471ffdceaa1a36c049b8c34
RPeGio <1121156227@qq.com>
2026-07-21 15:02:32 +0800

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -40,10 +40,10 @@ export default defineConfig({
 
   // [Adapter]
   // https://docs.astro.build/en/guides/deploy/
-  adapter: cloudflare(),
+  // adapter: cloudflare(),
   // Local (standalone)
   // adapter: node({ mode: 'standalone' }),
-  // output: 'server',
+  output: 'server',
 
   // [Assets]
   image: {
